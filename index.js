7dd55bc6e726cf46c86396d427f7b71e063071b1
Chris-Owusu <christopher.owusu@amalitech.com>
2023-01-17 11:54:20 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// current age
+let myAge = 20;
+// earlyYears
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+
+// laterYears
+let laterYears = myAge - 2
+laterYears *= 4
+
+// myAgeInDogYears
+myAgeInDogYears = earlyYears + laterYears
+
+let myName = 'Chris'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
